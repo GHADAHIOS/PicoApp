fe91720671d365a47aab65a22d044fcbf1854c27
AshwaqDev <ashwaq.jafer@gmail.com>
2024-12-12 11:50:50 +0300

    Initial Commit

---
+++ Pico/PicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicoApp.swift
+//  Pico
+//
+//  Created by Ashwaq on 11/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PicoTests/PicoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PicoTests.swift
+//  PicoTests
+//
+//  Created by Ashwaq on 11/06/1446 AH.
+//
+
+import Testing
+@testable import Pico
+
+struct PicoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
